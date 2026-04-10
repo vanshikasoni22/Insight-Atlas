b93a3a0f2a234a9c198a12564881e677e4aad2cc
vanshikasoni22 <vanshikasoni468@gmail.com>
2026-04-10 23:16:17 +0530

    section divison

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const EVENTS_API   = "http://localhost:3000/public/question/64511719-7718-4e51-8
 const INVOICES_API = "http://localhost:3000/public/question/27ecdfa8-a096-4a59-93d1-bc0b47f066f8.json";
 const REVIEWS_API  = "http://localhost:3000/public/question/43af4062-a891-4762-8916-621a9f4f21bc.json";
 const PRODUCTS_API = "http://localhost:3000/public/question/ab0d100e-01ba-4ff1-80cc-4a90fd7e8458.json";
+const state = {
+  orders:   { raw: [], filtered: [], cols: [], page: 1, sortCol: null, sortDir: 'asc', loaded: false },
+  events:   { raw: [], filtered: [], cols: [], page: 1, sortCol: null, sortDir: 'asc', loaded: false },
+  invoices: { raw: [], filtered: [], cols: [], page: 1, sortCol: null, sortDir: 'asc', loaded: false },
+  reviews:  { raw: [], filtered: [], cols: [], page: 1, sortCol: null, sortDir: 'asc', loaded: false },
+  products: { raw: [], filtered: [], cols: [], page: 1, sortCol: null, sortDir: 'asc', loaded: false },
+};
